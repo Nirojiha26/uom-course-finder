7db3d5ca4f1d9a8faf62ff9a752a102270aa7beb
Nirojiha <nironirojiha@gmail.com>
2025-11-24 15:33:22 +0530

    feat(auth): implement login, register, OTP APIs using axios

---
+++ Frontend/services/auth.ts
@@ -0,0 +1,7 @@
+import api from "./api";
+
+export const registerUser = (data: any) => api.post("/Auth/register", data);
+export const verifyOtp = (data: any) => api.post("/Auth/verify-email", data);
+export const loginUser = (data: any) => api.post("/Auth/login", data);
+export const forgotPassword = (data: any) => api.post("/Auth/forgot-password", data);
+export const resetPassword = (data: any) => api.post("/Auth/reset-password", data);
