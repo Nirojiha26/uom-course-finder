6ce35c97fb6ef0f49a64c643f7462f6385771027
Nirojiha <nironirojiha@gmail.com>
2025-11-24 15:29:20 +0530

    Integrated navigation into App.tsx

--- Frontend/App.tsx
+++ Frontend/App.tsx
@@ -1,17 +1,10 @@
-// App.tsx
-import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import HomeScreen from "./screens/HomeScreen";
-
-const Stack = createNativeStackNavigator();
+import RootNavigator from "./navigation/RootNavigator";
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
+      <RootNavigator />
     </NavigationContainer>
   );
 }
